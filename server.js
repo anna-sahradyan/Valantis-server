07d1fe6b5428629a56ad6876b0212865771fdc53
Anna Paruryan <a.sahradyan72@gmail.com>
2024-02-28 10:25:32 -0800

    I changed the command to start the server.

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const databaseConfig = {
   uri: process.env.MONGODB_URI,
   option: {},
 };
-
+fastify.register(fastifyCors, {
+  origin: ['*'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
 const startServer = async () => {
   try {
-    fastify.register(fastifyCors, {
-      origin: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    });
+
     await fastify.listen({
-      port: process.env.PORT || 4000,
+      port: process.env.PORT || 5000,
     });
     fastify.log.info(`Server is running on port ${fastify.server.address().port}`);
   } catch (err) {
@@ -28,19 +28,15 @@ const configuresRoutes = () => {
   fastify.register(userRoutes, { prefix: '/api/v1/users' });
 };
 const start = async () => {
-  const controller = new AbortController();
-  const { signal } = controller;
-
   try {
     await configurationDatabase(databaseConfig);
     configuresRoutes();
-    await startServer(signal);
+    await startServer();
   } catch (err) {
     console.log(err);
   }
   process.on('SIGINT', async () => {
     try {
-      controller.abort();
       await mongoose.connection.close();
       process.exit(0);
     } catch (err) {
